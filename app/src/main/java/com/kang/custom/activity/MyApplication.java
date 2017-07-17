c9d402ebeda33291218b14cf1373191fac55316f
kang <dk_dowork@163.com>
2017-07-17 14:41:55 +0800

    add global application

--- app/src/main/java/com/kang/custom/activity/MyApplication.java
+++ app/src/main/java/com/kang/custom/activity/MyApplication.java
@@ -1,20 +1,25 @@
 package com.kang.custom.activity;
 
 import android.app.Application;
-import android.content.Context;
 
 /**
  * Created by kang on 2017/7/15.
  */
 
 public class MyApplication extends Application {
-    private static Context context;
+    private static MyApplication myApp;
+
+    public MyApplication() {
+    }
 
     public void onCreate() {
         super.onCreate();
-        context = getApplicationContext();
     }
-    public static Context getContext(){
-        return context;
+
+    public MyApplication getInstance(){
+        if(myApp == null){
+            myApp = new MyApplication();
+        }
+        return myApp;
     }
 }
